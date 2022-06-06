630f8d948d36d8872baf25d26833f25b0af0ade4
dungdao14 <105336687+dungdao14@users.noreply.github.com>
2022-06-06 11:56:24 +0700

    Add files via upload

---
+++ PaymentHistoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InsuranceCard.Models;
+
+namespace InsuranceCard.Controllers
+{
+    public class PaymentHistoryController : Controller
+    {
+        List<Paymenthistory> list = null;
+        public IActionResult Index()
+        {
+            using(var context = new SWP391Context())
+            {
+                list = context.Paymenthistories.ToList();
+            }
+            return View(list);
+        }
+    }
+}
