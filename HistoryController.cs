9447fdc600103bbbacede3389a9869e2154ccd5f
Hoangvippro <105336688+Hoangvippro@users.noreply.github.com>
2022-06-10 09:07:49 +0700

    Add files via upload

---
+++ HistoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CardInsu.Models;
+
+namespace CardInsu.Controllers
+{
+    public class HistoryController1 : Controller
+    {
+        List<History> list = null;
+        public IActionResult Index()
+        {
+            using (var context = new SWP391Context())
+            {
+                list = context.Histories.ToList();
+            }
+            return View(list);
+        }
+    }
+}
