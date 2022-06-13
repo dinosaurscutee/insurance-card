0ff6d55a38d5a3ffac1d2883403117a47af5634b
trungdepzaii <105336703+trungdepzaii@users.noreply.github.com>
2022-06-13 08:12:54 +0700

    Add files via upload

---
+++ PersonalController1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BaoHiem.Models;
+using System.Linq;
+
+namespace BaoHiem.Controllers
+{
+    public class PersonalController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            var db = new SWP391Context();
+            CustomerUser a = new CustomerUser();
+            a = db.CustomerUsers.FirstOrDefault(x => x.CustomerId == 1);
+            return View(a);
+        }
+    }
+}
