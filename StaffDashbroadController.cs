101e0bac7f6efac02e8abd00beff319e111f45e6
dungdao14 <105336687+dungdao14@users.noreply.github.com>
2022-06-13 10:58:22 +0700

    Add files via upload

---
+++ StaffDashbroadController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InsuranceCardSWP301.Models;
+
+namespace InsuranceCardSWP301.Controllers
+{
+    public class StaffDashbroadController : Controller
+    {
+        List<Paymenthistory> list = null;
+        public IActionResult Index()
+        {
+            using (var context = new SWP391Context())
+            {
+                list = context.Paymenthistories.ToList();
+            }
+            return View(list);
+        }
+    }
+}
